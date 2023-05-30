8a7c809303140b72f3a39cce7bc44817a42d5783
Petergt <linkpj@gmail.com>
2023-05-30 14:26:22 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,6 +39,6 @@ function encriptar() {
   function duplicar() {
     var contenido = document.querySelector("#resultado");
     contenido.select();
-    document.execCommand('copiar');
-    alert("Copiado!");
+    document.execCommand("copy");
+    alert("Mensaje Copiado!");
   }
